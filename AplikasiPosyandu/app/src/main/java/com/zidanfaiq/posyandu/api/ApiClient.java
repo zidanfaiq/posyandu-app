d2e0c6810767f3149178903d41babd55e00c1228
zidanfaiq <zidanfaiq08@gmail.com>
2021-11-16 22:21:30 +0700

    base url

--- AplikasiPosyandu/app/src/main/java/com/zidanfaiq/posyandu/api/ApiClient.java
+++ AplikasiPosyandu/app/src/main/java/com/zidanfaiq/posyandu/api/ApiClient.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
-    private static final String BASE_URL = "http://192.168.34.141/API/";
+    private static final String BASE_URL = "https://user-posyandu-app.000webhostapp.com/";
 
     private static Retrofit retrofit;
 
